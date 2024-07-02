3f4d3eb71a93610fdefb64928246c37c4cea498e
Faisal-Github-Account <faysalshaykh0@gmail.com>
2024-07-02 06:43:38 +0500

    Add files via upload

---
+++ 14 Guest_List.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_List :string[] = ['imran khan', 'Nawaz Sharif', 'Asif Ali Zardari',];
+
+for(let i=0; i<Guest_List.length; i++){
+
+ console.log('Dear Mr.' + Guest_List[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank You!')
+}
