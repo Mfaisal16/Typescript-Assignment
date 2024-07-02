44a8bba6b8e014a52eefcaba4910ac257b9850c1
Faisal-Github-Account <faysalshaykh0@gmail.com>
2024-07-02 06:42:14 +0500

    Add files via upload

---
+++ 12 Greeting.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['faysal', 'hasan', 'rizwan', 'saqib'] ;
+
+const message :string = 'Welcome to Typescript class: '
+
+for (let i=0; i<names.length; i++){
+
+console.log(message + names[i]) ;
+
+}
